63cd21a7c030ca2c1e51910a4ca0c7f05c188dcb
Daniel Park <ekklesia89@gmail.com>
2019-11-23 22:18:07 +0900

    ADD request extension & refresh token

--- index.ts
+++ index.ts
@@ -7,27 +7,66 @@ const app: Application = express();
 
 app.use(express.json());
 
+// mocking DB
+const userTable: { user: string; password: string; refreshToken: any } = {
+  user: "test@test.com",
+  password: "qwe123",
+  refreshToken: null
+};
+
+const isAuthorized = (req: Request, res: Response, next: NextFunction): any => {
+  if (typeof req.headers.authorization !== "undefined") {
+    const token: string = req.headers.authorization.split(" ")[1];
+    const secret: string = process.env.privateKey;
+    const refresh: string = process.env.refreshKey;
+
+    jwt.verify(token, secret, (err, decoded) => {
+      if (err) {
+        return res.send("token expired");
+      }
+      console.log("decoded", decoded);
+      return next();
+    });
+  } else {
+    res.status(500).json({ error: "Not Authorized" });
+  }
+};
+
+const generateToken = (user: { email: string }): string => {
+  const secret: string = process.env.privateKey;
+  const token: string = jwt.sign({ email: user.email }, secret, {
+    expiresIn: "30s"
+  });
+  return token;
+};
+
+const generateRefreshToken = (user: string): string => {
+  const refresh: string = process.env.refreshKey;
+  const token: string = jwt.sign(user, refresh);
+  return token;
+};
+
 app.get("/", (req: Request, res: Response) => {
   res.send("hello world");
 });
 
-app.get("/secret", (req: Request, res: Response) => {
+app.get("/secret", isAuthorized, (req: Request, res: Response) => {
   res.json({ body: "hello, sir" });
 });
 
-app.get("/jwt", (req: Request, res: Response) => {
-  const secret: string = process.env.privateKey;
-
-  const token: string = jwt.sign({ user: "superUser" }, secret);
-
-  console.log("token created:", token);
-  res.json(token);
-});
-
-const isVerified = (req: Request, res: Response, next: NextFunction) => {
-  if (req.headers.authorization !== "undefined") {
+app.post("/login", (req: Request, res: Response) => {
+  if (
+    userTable.user === req.body.email &&
+    userTable.password === req.body.password
+  ) {
+    const refreshToken: string = generateRefreshToken(userTable.user);
+    userTable.refreshToken = refreshToken;
+    const token: string = generateToken({ email: userTable.user });
+    res.json({ token, refreshToken });
+  } else {
+    res.status(403).json({ error: "email or password is wrong" });
   }
-};
+});
 
 app.listen(3000, () => {
   console.log("listening on 3000 ...");
